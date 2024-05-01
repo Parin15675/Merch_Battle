80fa82bd4df99713e474cd340990c127cc35b9ed
Parin15675 <149325918+Parin15675@users.noreply.github.com>
2024-05-01 21:05:24 +0700

    set posZ

--- Assets/Scripts/Draggable.cs
+++ Assets/Scripts/Draggable.cs
@@ -1,23 +1,47 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
+    private Transform parentAfterDrag;
+    private Vector3 startPosition;
+    public GameManeger gameManager; // Reference to the GameManager
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("Begin drag");
+        parentAfterDrag = transform.parent;
+        startPosition = transform.position; // Store start position to preserve z value
+        transform.SetParent(transform.root);
+        transform.SetAsLastSibling();
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         Debug.Log("Dragging");
-        transform.position = Input.mousePosition;
+        transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, startPosition.z);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("End drag");
+        transform.SetParent(parentAfterDrag);
+        transform.position = new Vector3(transform.position.x, transform.position.y, startPosition.z);
+
+        gameManager.NewGame();
+
+    }
+
+    private bool IsOverGameBoard(PointerEventData eventData)
+    {
+        // Assuming you have some way to detect if over the game board
+        // You might need a collider or a specific tag to identify the game board area
+        if (eventData.pointerCurrentRaycast.gameObject != null &&
+            eventData.pointerCurrentRaycast.gameObject.CompareTag("GameBoard"))
+        {
+            return true;
+        }
+        return false;
     }
 }
