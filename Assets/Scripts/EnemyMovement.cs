262fcc69a1d79da4294e46062afc1784b652c4de
Parin15675 <149325918+Parin15675@users.noreply.github.com>
2024-05-03 00:33:40 +0700

    attack health

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed = -30.0f; // Speed at which the hero should move
+    private bool canMove = true; // Flag to control movement
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (canMove)
+        {
+
+            float moveAmount = speed * Time.deltaTime;
+            transform.Translate(new Vector3(moveAmount, 0, 0));
+        }
+        else
+        {
+
+
+            transform.Translate(new Vector3(0, 0, 0));
+        }
+    }
+
+    // Public method to stop the hero
+    public void StopMovement()
+    {
+        canMove = false;
+        speed = 0;
+    }
+
+    public void StartMovement(float newSpeed)
+    {
+        speed = -30.0f; // Set new speed if needed
+        canMove = true;
+        Debug.Log("Movement restarted at speed: " + newSpeed);
     }
 }
