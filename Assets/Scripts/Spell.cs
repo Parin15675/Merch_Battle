c2ebc53229e1878d2e4a4674e5fb39e66ab6551f
Parin Vessakosol <149325918+Parin15675@users.noreply.github.com>
2024-05-09 14:40:10 +0700

    Spell

--- Assets/Scripts/Spell.cs
+++ Assets/Scripts/Spell.cs
@@ -1,16 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Spell : MonoBehaviour
 {
+    private Rigidbody2D spellRigidBody;
+    private Transform target;
+    private GameObject spellAim;
+    private GameObject spellObj;
+    private Vector3 startPosition;
+    private Vector3 lastAimPosition;
+
+    [SerializeField]
+    private float speed;
+
+    public GameObject spellPrefab;
+    public GameObject spellAimPrefab;
+
     public void Start()
     {
-        gameObject.SetActive(false);
+        
     }
 
-    public void Setup()
+    public void cast()
     {
-        gameObject.SetActive(true);
+        if (spellAim == null) 
+        {
+            spellAim = Instantiate(spellAimPrefab, transform);
+            startPosition = spellAim.transform.position;
+        }
+    }
+
+    public void Update()
+    {
+        
+        if (!Input.GetMouseButtonDown(0) && spellAim != null && spellObj == null)
+        {
+            spellAim.transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, startPosition.z);
+            lastAimPosition = spellAim.transform.position;
+        } 
+        else if (Input.GetMouseButtonDown(0) && spellAim != null) 
+        {
+            spellObj = Instantiate(spellPrefab, new Vector3(lastAimPosition.x, lastAimPosition.y + 1000, 1), spellPrefab.transform.rotation);
+            spellObj.transform.SetParent(spellAim.transform.root);
+            spellObj.transform.SetAsLastSibling();
+        } 
+        else if(spellObj != null && spellObj.transform.position.y < lastAimPosition.y)
+        {
+            Destroy(spellAim);
+            Destroy(spellObj);
+        }
     }
 }
