1e0a4c39f5e3fd4fef783d60bd3d3d136dec4d98
Aekkee <satikit@gmail.com>
2024-05-17 14:58:59 +0700

    ice spell

--- Assets/Scripts/Spell/FrostAbility.cs
+++ Assets/Scripts/Spell/FrostAbility.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 
 public class FrostAbility : Ability
 {
-    public int HealingAmount = 1;
+    public GameObject icePrefab; // Ice prefab to be spawned
+    private List<EnemyMovement> affectedEnemies = new List<EnemyMovement>();
+    private Dictionary<EnemyMovement, GameObject> enemyIceInstances = new Dictionary<EnemyMovement, GameObject>();
 
     private void modifyStartPosition(Vector3 parentPos)
     {
@@ -24,25 +26,62 @@ public class FrostAbility : Ability
         modifyStartPosition(parent.transform.position);
     }
 
-
     public void OnTriggerEnter2D(Collider2D target)
     {
-        if (target.gameObject.CompareTag("Hero"))
+        if (target.gameObject.CompareTag("Enemy"))
         {
-            Debug.Log(gameObject.name + "Heal");
-            Health heroHealth = target.gameObject.GetComponent<Health>();
-            StartCoroutine(Healing(heroHealth));
+            EnemyMovement enemyMovement = target.gameObject.GetComponent<EnemyMovement>();
+            if (enemyMovement != null && !affectedEnemies.Contains(enemyMovement))
+            {
+                affectedEnemies.Add(enemyMovement);
+                StartCoroutine(Frosting(enemyMovement));
+            }
         }
     }
 
-    private IEnumerator Healing(Health heroHealth)
+    public void OnTriggerExit2D(Collider2D target)
     {
-
-        while (heroHealth.currentHealth > 0)
+        if (target.gameObject.CompareTag("Enemy"))
         {
-            heroHealth.Heal(HealingAmount);
-            yield return new WaitForSeconds(0.1f);
+            EnemyMovement enemyMovement = target.gameObject.GetComponent<EnemyMovement>();
+            if (enemyMovement != null && affectedEnemies.Contains(enemyMovement))
+            {
+                affectedEnemies.Remove(enemyMovement);
+                enemyMovement.enabled = true;
+
+                if (enemyIceInstances.ContainsKey(enemyMovement))
+                {
+                    Destroy(enemyIceInstances[enemyMovement]);
+                    enemyIceInstances.Remove(enemyMovement);
+                }
+            }
+        }
+    }
+
+    private IEnumerator Frosting(EnemyMovement enemyMovement)
+    {
+        // Disable enemy movement
+        enemyMovement.enabled = false;
+
+        // Spawn ice prefab at the enemy's position
+        GameObject iceInstance = Instantiate(icePrefab, enemyMovement.transform.position, Quaternion.identity);
+        iceInstance.transform.SetParent(enemyMovement.transform);
+        enemyIceInstances[enemyMovement] = iceInstance;
+
+        // Wait for 5 seconds
+        yield return new WaitForSeconds(5f);
+
+        // Enable enemy movement
+        enemyMovement.enabled = true;
+
+        // Destroy the ice instance
+        if (enemyIceInstances.ContainsKey(enemyMovement))
+        {
+            Destroy(enemyIceInstances[enemyMovement]);
+            enemyIceInstances.Remove(enemyMovement);
         }
 
+        // Remove from affected enemies list after the effect is over
+        affectedEnemies.Remove(enemyMovement);
     }
 }
