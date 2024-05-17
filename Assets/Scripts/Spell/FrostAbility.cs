0bcf369b1f6df1d8354e078919309f82c7eb22a3
Aekkee <satikit@gmail.com>
2024-05-17 14:23:14 +0700

    aek

---
+++ Assets/Scripts/Spell/FrostAbility.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FrostAbility : Ability
+{
+    public int HealingAmount = 1;
+
+    private void modifyStartPosition(Vector3 parentPos)
+    {
+        gameObject.transform.position = parentPos;
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public override void Activate(GameObject parent)
+    {
+        transform.SetParent(parent.transform);
+        transform.SetAsLastSibling();
+        modifyStartPosition(parent.transform.position);
+    }
+
+
+    public void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.gameObject.CompareTag("Hero"))
+        {
+            Debug.Log(gameObject.name + "Heal");
+            Health heroHealth = target.gameObject.GetComponent<Health>();
+            StartCoroutine(Healing(heroHealth));
+        }
+    }
+
+    private IEnumerator Healing(Health heroHealth)
+    {
+
+        while (heroHealth.currentHealth > 0)
+        {
+            heroHealth.Heal(HealingAmount);
+            yield return new WaitForSeconds(0.1f);
+        }
+
+    }
+}
